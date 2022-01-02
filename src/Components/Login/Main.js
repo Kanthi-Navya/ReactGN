f314ef8fd54d0b72eb32c7664b3c5cc68caba6cd
Kanthi navya <kanthi.navyarani@gmail.com>
2022-01-02 14:43:01 +0530

    main login page view

--- src/Components/Login/Main.js
+++ src/Components/Login/Main.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import { Row, Col, Container } from 'react-bootstrap';
-// import InputGroup from "react-bootstrap/InputGroup";
-// import {FormGroup} from "react-bootstrap/Form";
-// import {FormControl} from "react-bootstrap/Form";
+import React, { useState } from 'react';
+import { Row, Col, Container, Button } from 'react-bootstrap';
 import { Form, FormGroup } from "react-bootstrap";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import Input from 'react-validation/build/input';
-
-
+import Google from '../../images/Google.svg';
+import facebook from '../../images/facebook.svg';
+import apple from '../../images/apple.svg';
 import { faCircle, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
 
 const Main = () => {
   return (
@@ -24,24 +22,45 @@ const Main = () => {
         </div>
         <div>
           <Row>
-            <>
-
-              <InputGroup>
-                <FormControl aria-label="Text input with radio button" />
-                <InputGroup.Radio aria-label="Radio button for following text input " disabled="true"/>
-                {/* <button className="btn" type="button">
-                  <FontAwesomeIcon icon={faCircle} />
-                </button> */}
+            <Col sm={5}>
+              <InputGroup className="login_group">
+                <FormControl placeholder="Teacher / Student ID" />
+                <InputGroup.Text >
+                  <button className="btn" type="button">
+                    <FontAwesomeIcon icon={faCircle} />
+                  </button>
+                  {/* <InputGroup.Radio aria-label="Radio button for following text input"/> */}
+                </InputGroup.Text>
               </InputGroup>
-              <InputGroup>
-                <FormControl aria-label="Text input with radio button" />
-                {/* <InputGroup.Radio aria-label="Radio button for following text input " disabled="true"/> */}
-                <button className="btn" type="button">
-                  <FontAwesomeIcon icon={faEyeSlash} />
-                </button>
+              <InputGroup className="login_group">
+                <FormControl placeholder="8 Digit Pin" />
+                <InputGroup.Text>
+                  <button className="btn" type="button">
+                    <FontAwesomeIcon icon={faEyeSlash} />
+                  </button>
+                </InputGroup.Text>
               </InputGroup>
-              {/* <Input value="" name='Id' placeholder="8 Digit Pin" className="login-input"/> */}
-            </>
+              <Button className="loginBtn btn-lg">Login to your account </Button>
+            </Col>
+            <Col sm={2} xl={1} className="separator">/</Col>
+            <Col sm={5}>
+              <InputGroup className="login_group ">
+                <Button className="loginBtns btn-lg">
+                  <img src={Google} alt="G" />
+                  <span>Sign in with Google</span>
+                </Button>
+              </InputGroup>
+              <InputGroup className="login_group ">
+                <Button className="loginBtns btn-lg">
+                  <img src={facebook} alt="f" />
+                  <span>Sign in with Facebook</span>
+                </Button>
+              </InputGroup>
+              <Button className="loginBtns btn-lg">
+                <img src={apple} alt="apple" />
+                <span>Sign in with Apple Account</span>
+              </Button>
+            </Col>
           </Row>
         </div>
       </Container>
